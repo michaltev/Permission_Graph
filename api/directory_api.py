67305622c4b6708364989e4b4f1756afff788e0f
michaltev <michaltev@gmail.com>
2020-06-07 16:24:56 +0300

    returns the values of the fetch (not printing)

--- api/directory_api.py
+++ api/directory_api.py
@@ -21,51 +21,32 @@ class DirectoryAPI:
         self.service = build('admin', 'directory_v1', credentials=credentials)
 
     def fetch_users_in_organization(self):
-        # Call the Admin SDK Directory API
         results = self.service.users().list(customer='my_customer').execute()
         users = results.get('users', [])
-
-        if not users:
-            print('No users in the domain.')
-        else:
-            print('Users:')
-            for user in users:
-                print(u'{0} ({1})'.format(user['primaryEmail'],
-                                          user['name']['fullName']))
-        pass
+        return users
 
     def fetch_groups_in_organization(self):
-        # Call the Admin SDK Directory API
         results = self.service.groups().list(customer='my_customer').execute()
         groups = results.get('groups', [])
-
-        if not groups:
-            print('No groups in the domain.')
-        else:
-            print('Groups:')
-            for group in groups:
-                print(u'{0} - {1}({2} members)'.format(group['email'],
-                                                       group['name'],
-                                                       group['directMembersCount']))
-        pass
+        return groups
 
     def fetch_users_in_groups(self):
-        # groups -> members -> member.type -> member.email
-        results = self.service.groups().list(customer='my_customer').execute()
-        groups = results.get('groups', [])
+        groups = self.fetch_groups_in_organization()
+        dict_users_in_groups = {}
 
-        if not groups:
-            print('No groups in the domain')
-        else:
+        if groups:
             for group in groups:
-                print(u'GROUP {0}({1})'.format(group['email'], group['name']))
+                group_id = group['id']
+                group_name = group['name']
+                lst_users_members = self.get_group_members(group_id)
+                dict_users_in_groups[group_name] = lst_users_members
+        return dict_users_in_groups
 
-                results = self.service.members().list(groupKey=group['id']).execute()
-                members = results.get('members', [])
-                if not groups:
-                    print('No members in the group')
-                else:
-                    for member in members:
-                        if member['type'] == "USER":
-                            print(u'USER {0} is a member'.format(member['email']))
-        pass
+    def get_group_members(self, group_id):
+        results = self.service.members().list(groupKey=group_id).execute()
+        members = results.get('members', [])
+        lst_users_members = []
+        if members:
+            lst_users_members = [member['email'] for member in members if
+                                 member['type'] == "USER"]
+        return lst_users_members
